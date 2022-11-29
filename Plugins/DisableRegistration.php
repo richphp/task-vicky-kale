f91e0e5a5bec78f3d2a22e8170f5b068118745ac
vicky <kalevickyn@gmail.com>
2022-11-29 17:39:07 +0530

    Initial Commit

---
+++ Plugins/DisableRegistration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CustomModule\midTermExercise\Plugins;
+
+use Magento\Customer\Model\Registration;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class DisableRegistration{
+    protected $scopeConfig;
+    const XML_DISABLE_CUSTOMER_REGISTRATION = 'customer/create_account/disable_customer_registration';
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    )
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+
+    public function afterIsAllowed( Registration $subject, $result)
+    {
+        if($this->scopeConfig->isSetFlag(
+            self::XML_DISABLE_CUSTOMER_REGISTRATION, 
+            $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT, 
+            $scopeCode = null)
+        ){
+            return false;
+        }else{
+            return true;
+        }
+
+        return $result;
+    }
+}
+
